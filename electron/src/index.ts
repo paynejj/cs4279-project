c58c6022b4a3c51b035cd71a3a1d0e023293099f
Joshua-J-Payne <joshua.j.payne@vanderbilt.edu>
2023-01-17 13:22:54 -0600

    Initial Commit

--- electron/src/index.ts
+++ electron/src/index.ts
@@ -17,7 +17,7 @@ const createWindow = () => {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join('../index.html'));
+  mainWindow.loadFile(path.join('./index.html'));
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
