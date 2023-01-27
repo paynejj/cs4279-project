cba03c1960369a893cee068d9df79cdba31103d4
Joshua-J-Payne <joshua.j.payne@vanderbilt.edu>
2023-01-27 15:55:08 -0600

    New Initial Commit with React and TypeScript

--- project/public/electron.ts
+++ project/public/electron.ts
@@ -1,32 +1,35 @@
-const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) {
-  app.quit();
-}
-//beans 
-const createWindow = () => {
+const { app, BrowserWindow } = require('electron');
+const isDev = require('electron-is-dev');
+
+function createWindow() {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
     },
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join('./index.html'));
-
+  // win.loadFile("index.html");
+  win.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`
+  );
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-};
+  if (isDev) {
+    win.webContents.openDevTools({ mode: 'detach' });
+  }
+}
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.whenReady().then(createWindow);
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
@@ -38,12 +41,7 @@ app.on('window-all-closed', () => {
 });
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
