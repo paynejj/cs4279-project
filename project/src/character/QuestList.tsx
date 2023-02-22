674685177c05a0881666c9024c36db567a08f26d
wuy54 <yu.heng.wu@vanderbilt.edu>
2023-02-22 08:29:17 -0600

    More Polished CharcUI

--- project/src/character/QuestList.tsx
+++ project/src/character/QuestList.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import { Grid, Box, Button, Typography, Modal } from '@mui/material';
-import ConstructionIcon from '@mui/icons-material/Construction';
 
 const style = {
     position: 'absolute' as 'absolute',
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 400,
+    width: '50%',
+    height: '50%',
     bgcolor: 'black',
     border: '2px solid purple',
     boxShadow: 24,
@@ -24,15 +24,67 @@ const spellASCII = `
  '---'
 `;
 
+function createQuest(name = '', description = '') {
+    return { name, description }
+}
+
+const questRow = [
+    createQuest(
+        'Get Some French Fries',
+        `You may be wondering what the quest is about... welllllll it is nott about anything really that is worth mentioning. \
+        This is what the quest is about:\nGo get some french fries from \n BurgerQueen.\n`
+    ),
+    createQuest(
+        'Look under the table',
+        'Just get your knees down there mate...'
+    ),
+    createQuest(
+        'One Bite Big Mac',
+        'As title.'
+    ),
+    createQuest(
+        'Stick Your Head OUT',
+        'Stick your head out of the window in the basement. As simple as it is.'
+    ),
+]
+
+
 export default function QuestListModal() {
     const [open, setOpen] = React.useState(false);
+    const [questDescription, setQuestDescription] = React.useState(questRow[0].description);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
+
+    function clickQuest(id, idx) {
+        for (let i = 0; i < questRow.length; ++i) {
+            let element = document.getElementById("quest" + questRow[i].name);
+            if (element !== null) {
+                if (questRow[i].name != id) {
+                    element.style.color = "pink";
+                    element.style.backgroundColor = "black";
+                } else {
+                    element.style.color = "black";
+                    element.style.backgroundColor = "purple";
+                    setQuestDescription(questRow[idx].description);
+                }
+
+            }
+
+
+        }
+
+
+
+    }
 
     return (
         <div>
             <Button
-                sx={{ color: "pink", textAlign: "left" }}
+                sx={{ color: "pink", textAlign: "left",
+                ':hover': {
+                    bgcolor: 'purple',
+                    color: 'black',
+                }, }}
                 onClick={handleOpen}
                 style={{
                     maxWidth: '60px', maxHeight: '110px',
@@ -49,7 +101,7 @@ export default function QuestListModal() {
                 aria-describedby="modal-modal-description"
             >
                 <Box sx={style}>
-                    <Grid container>
+                    <Grid container sx={{ height: '80%' }}>
                         <Grid item xs={12}>
                             <Typography id="modal-modal-title" variant="h6" component="h2"
                                 sx={{ fontFamily: 'serif', fontSize: '36px' }}>
@@ -57,14 +109,53 @@ export default function QuestListModal() {
                             </Typography>
                         </Grid>
 
-                        <Grid item sx={{ mt: 2 }} xs={8}>
-                            <Button sx={{ color: "pink"}}
-                            >
-                                <ConstructionIcon display="inline" sx={{color: 'pink'}}/>
-                                <Typography display="inline" sx={{color: 'pink'}}>
-                                    The quest list is under construction...
-                                </Typography>
-                            </Button>
+                        <Grid item sx={{ mt: 2, height: '100%' }} xs={12}>
+
+                            <Grid container columns={12} sx={{ height: '100%' }}>
+                                <Grid item xs={4} columns={12} sx={{ height: '100%', overflowY: 'auto' }}>
+                                    {questRow.map((row, idx) => (
+                                        <Grid item xs={12} key={row.name}>
+                                            <Typography
+                                                id={"quest" + row.name}
+                                                variant="h6"
+                                                lineHeight="2.5"
+                                                fontSize="18px"
+                                                fontStyle="italic"
+                                                width="90%"
+                                                onClick={() => clickQuest(row.name, idx)}
+                                                sx={{
+                                                    color: "pink",
+                                                    ':hover': {
+                                                        bgcolor: 'purple',
+                                                        color: 'black',
+                                                    },
+                                                }}
+                                            >
+                                                {row.name}
+                                            </Typography>
+                                        </Grid>
+                                    ))}
+                                </Grid>
+                                <Grid item xs={7} sx={{ height: '100%', overflowY: 'auto' }}>
+                                    {(questDescription.split(/\n/)).map((row, idx) => (
+                                        <Typography
+                                            key={idx}
+                                            text-align='justify'
+                                            sx={{ overflowX: 'hidden', }}>
+                                            {row}
+                                        </Typography>
+
+
+                                    ))}
+                                </Grid>
+                            </Grid>
+
+
+
+
+
+
+
                         </Grid>
                     </Grid>
 
