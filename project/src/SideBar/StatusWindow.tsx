73db36e214791073701e7a383077e2a417ef7389
Joshua-J-Payne <joshua.j.payne@vanderbilt.edu>
2023-04-18 06:40:16 -0500

    Status Window Complete

--- project/src/SideBar/StatusWindow.tsx
+++ project/src/SideBar/StatusWindow.tsx
@@ -1,22 +1,28 @@
 import Window from "floating-window-ui";
 import { useContext } from "react";
 import { PlayerDataContext } from "../Player/PlayerDataContext";
+import { purple } from "@mui/material/colors";
 
 function StatusWindow() {
 
-    const {playerData, setPlayerData} = useContext(PlayerDataContext)
+    const { playerData, setPlayerData } = useContext(PlayerDataContext)
     return (
         <Window
             id="react-window"
-            height={200}
-            width={150}
+            height={300}
+            width={300}
+            style={{backgroundColor: "black"}}
             resizable={true}
             titleBar={{
                 icon: "⚛",
                 title: "Status",
                 buttons: { minimize: true },
             }}>
-                <p> {JSON.stringify(playerData)}</p>
+            <ul style={{color: "purple", backgroundColor: "black", height: "100%"}}>
+                <li><h1>{playerData.name}</h1></li>
+                <li>Gold: {playerData.gold}</li>
+                {Object.entries(playerData.stats).map((e) => <li key={e[0]}>{e[0]}: {e[1]}</li>) }
+            </ul>
         </Window>
     )
 }
